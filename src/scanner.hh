da92d82e905114d111bfd77e143bce2a33a31933
João Paulo Lazzarini Cyrino <jpcyrino@gmail.com>
2022-06-30 15:25:54 -0300

    header file adjustments

--- src/scanner.hh
+++ src/scanner.hh
@@ -9,27 +9,12 @@
 namespace Lox
 {
 
-    
-
 class Scanner {
     std::vector<Token> tokens;
-    std::string source;
+    const std::string source;
     std::string buffer;
     size_t position = 0;
     int current_line = 1;
-
-    void get_token();
-    bool peek(char next_char);
-    bool is_number();
-    bool is_separator();
-    void add_single(TokenType token_type, std::string token_name);
-    void add_double(TokenType token_type, std::string token_name);
-    void handle_comment();
-    void handle_numeric_literal();
-    void handle_string_literal();
-    void handle_lexical_item();
-    void add_lexical_item(std::string item);
-
     const std::unordered_map<std::string, TokenType> keywords{
         {"AND", AND},
         {"CLASS", CLASS},
@@ -49,12 +34,23 @@ class Scanner {
         {"WHILE", WHILE}
     };
 
-public:
-    explicit Scanner(std::string source)  { this->source = std::move(source); }
+    void get_token();
+    bool peek(char next_char);
+    bool is_number();
+    bool is_separator();
+    void add_single(TokenType token_type, std::string token_name);
+    void add_double(TokenType token_type, std::string token_name);
+    void handle_comment();
+    void handle_numeric_literal();
+    void handle_string_literal();
+    void handle_lexical_item();
+    void add_lexical_item(std::string item);
 
+public:
+    Scanner(std::string source) : source(std::move(source)) { }
     const std::vector<Token>& get_tokens() const { return tokens; }
     void start_scan();
-
+    
 };
 
 
